1a312b9aba4df56afacb72e38c9af1aa8bdd15da
Robert Broersma <robertbroersma@gmail.com>
2020-03-13 11:32:16 +0100

    nvm google prefetch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Josefin Sans`,
-            variants: ['300', '400', '600'],
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Josefin Sans`,
+    //         variants: ['300', '400', '600'],
+    //       },
+    //     ],
+    //   },
+    // },
   ],
 }
